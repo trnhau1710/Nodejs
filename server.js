9cd893a385660a20d42d601b91b1a8035f4b4f0b
trnhau1710 <hauvtct@gmail.com>
2026-01-24 16:05:03 +0700

    NPM

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
 
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end('Hello World\n Tran Huu Hau');
 });
 
 server.listen(port, hostname, () => {
