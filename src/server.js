8dd29dd199eb3e99ae02b495728b26ffe4841db3
trnhau1710 <hauvtct@gmail.com>
2026-01-25 13:41:22 +0700

    update npm

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+require('dotenv').config(); //để dùng được process.env.PORT
+
+let app = express();
+// config app
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+viewEngine(app);
+initWebRoutes(app);
+
+
+let port = process.env.PORT || 6969; //Port == undefined => Port = 6969
+app.listen(port, () => {
+    console.log("Backend nodejs is running on the port: " + port);
+})
