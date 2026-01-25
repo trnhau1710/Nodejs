0e85ec6306137ac92a17a2320f390a2fa019b538
trnhau1710 <hauvtct@gmail.com>
2026-01-25 18:05:07 +0700

    'export_data_to_views'

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB"
 require('dotenv').config(); //để dùng được process.env.PORT
 
 let app = express();
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
-
+connectDB(app);
 
 let port = process.env.PORT || 6969; //Port == undefined => Port = 6969
 app.listen(port, () => {
