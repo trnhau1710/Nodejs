34ee36949ac61180876de626b7cb752f31136210
trnhau1710 <hauvtct@gmail.com>
2026-01-25 17:17:58 +0700

    'update'

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,14 @@
 import express from "express";
-
+import homeController from "../controllers/homeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("Hello World")
-    });
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+
+    router.get('/hoihau', (req, res) => {
+        return res.send("Hello World with hau")
+    })
     return app.use("/", router);
 }
 
